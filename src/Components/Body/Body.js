7e6759796b22d5a4e5f941403af96494daa10796
nhn1998 <nhnayeem1@gmail.com>
2022-09-29 19:11:45 +0600

    interation

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -5,16 +5,21 @@ import './Body.css'
 import Blog from '../Blog/Blog';
 const Body = () => {
     const [infos,setInfos]=useState([])
+    const [carts,setCarts]=useState(0)
     useEffect(()=>{
         fetch('info.json')
         .then(res=>res.json())
         .then(data=>setInfos(data))
     },[])
+    const clickToAddCart=(info)=>{
+        const newCart= carts+parseInt(info.time);
+        setCarts(newCart)
+    }
     return (
         <div className='body-container'>
             <div className="product-container">
             {
-                infos.map(info=><Exercise key={info.id} info={info}></Exercise>)
+                infos.map(info=><Exercise key={info.id} info={info} clickToAddCart={clickToAddCart}></Exercise>)
             }
             </div>
             <div className='cart-container'>
@@ -46,11 +51,12 @@ const Body = () => {
                         <button>20s</button>
                         <button>30s</button>
                         <button>40s</button>
+                        <button>50s</button>
                     </div>
                 </div>
                 <div>
                     <h2>Exercise Details</h2>
-                    <h4>Exercise Time: </h4>
+                    <h4>Exercise Time: {carts}Mins</h4>
                     <h4>Break Time: </h4>
                 </div>
             </div>
--- src/Components/exercise/Exercise.js
+++ src/Components/exercise/Exercise.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Exercise.css'
 const Exercise = (props) => {
+    const {clickToAddCart}=props;
     const {name,img, time}=props.info
+
     return (
         <div className='img'>
             <img src={img} alt="" />
             <h4 className='info'>{name}</h4>
             <p className='info'>Time required: {time}</p>
-            <button className='btn'>Add To List</button>
+            <button onClick={()=>clickToAddCart(props.info)} className='btn'>Add To List</button>
         </div>
     );
 };
