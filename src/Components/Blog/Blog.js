79d1c962d5a2bea1d75e855159256c9a25c4817a
nhn1998 <nhnayeem1@gmail.com>
2022-09-29 16:35:09 +0600

    blog finished

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import './Blog.css'
 const Blog = () => {
     return (
-        <div>
-            <h2>This is blog item</h2>
+        <div className='blog'>
+            <h1>Questions:</h1>
+            <h2>1.How does react works?</h2>
+            <p>Ans: ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</p>
+            <h2>2.Difference between props and state</h2>
+            <p>Ans: State is a hook.if we want to do interaction.and we can send data by state.And other side which data we send those data will recive props.Props is receving data</p>
+            <h2>3.how useEffect works for?</h2>
+            <p>Ans:  you tell React that your component needs to do something after render. React will remember the function you passed (we'll refer to it as our “effect”), and call it later after performing the DOM updates.</p>
         </div>
     );
 };
