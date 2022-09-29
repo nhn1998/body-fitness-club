88e16e9596cf392518e99bc39c23acc8e3eb3de4
nhn1998 <nhnayeem1@gmail.com>
2022-09-29 16:21:35 +0600

    blog item

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Exercise from '../exercise/Exercise';
+import image from '../../nayeem.png'
 import './Body.css'
+import Blog from '../Blog/Blog';
 const Body = () => {
     const [infos,setInfos]=useState([])
     useEffect(()=>{
@@ -15,9 +17,30 @@ const Body = () => {
                 infos.map(info=><Exercise key={info.id} info={info}></Exercise>)
             }
             </div>
-            <div className="cart-container">
-                <h2>This is cart</h2>
+            <div className='cart-container'>
+            <div>
+                 <img src={image} alt="" />
             </div>
+            <div className='name-adress'>
+                <h3>Nayeem</h3>                     
+                <p>Dhaka,Bangladesh</p>
+            </div>
+                <div className='weight'>
+                <div className='Weight'>
+                <h2>73<small>kg</small></h2>
+                <p>Weight</p>
+                </div>
+                <div>
+                    <h2>5.6</h2>
+                    <p>Height</p>
+                </div>
+                    <div>
+                        <h2>24<small>yrs</small></h2>
+                        <p>Age</p>
+                    </div>
+                </div>
+            </div>
+            <Blog></Blog>
         </div>
 
     );
